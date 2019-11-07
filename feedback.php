a36c6bbe52ce17b35898a689daa242e0f053ff12
adityasarwankar <42802074+adityasarwankar@users.noreply.github.com>
2019-11-07 18:56:37 +0530

    1st

---
+++ feedback.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>HOME Tech Reviewer</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/style1.css">
+<link rel="stylesheet" href="css/style2.css">
+<link rel="stylesheet" href="feedbackform.css">
+  <link rel="icon" href="tr1.jpg" type="image/gif" sizes="16x16">
+</head>
+<body>
+<div class="header">
+  <h1>TECH-REVIEWER<i class="fa fa-mobile-phone" style="font-size:48px;color:white"></i></h1>
+  <p style="font-size:70%;"><u>TECH THAT MATTERS</u></p>
+</div>
+
+<br>
+<br>
+<form class="box" action="feedback.php" method="post">
+    <select name="SORT" style="width : 200px; cursor: pointer;" required>
+    <option value="times">SORT BY (time)</option>
+    <option value="RATING">RATE</option>
+    <option value="BRAND">BRAND</option>
+    </select>
+    <input type="submit" name="skip_Submit" value="Submit">
+</form>
+<br>
+<br>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "review";
+$SORT = $_POST['SORT'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if($SORT== NULL){
+    $SORT="times";
+}
+$sql = "SELECT NAME, EMAIL, BRAND, MODEL, RATING, REVIEW, times FROM con ORDER BY $SORT";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table id='customers'><tr><th>TIME</th><th>NAME</th><th>EMAIL</th><th>Brand</th><th>Model</th><th>Rating</th><th>Review</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["times"]. "</td><td>" . $row["NAME"]. "</td><td>" . $row["EMAIL"]. "</td><td> " . $row["BRAND"]. "</td> <td> " . $row["MODEL"]. "</td> <td> " . $row["RATING"]. "</td> <td> " . $row["REVIEW"]. "</td></tr>";
+    }
+    echo "</table>";
+    
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
+<div class="footer">
+        <div>	<h3>&copy; 2019 TechReviewer.com <img src="tr1.jpg" width="30px">	<hr STYLE="border: 1px solid white; width:50%;">
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-google"></a>
+        </div>
+      </div>
+</body>
+</html>
